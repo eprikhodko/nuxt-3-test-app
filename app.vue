3a260584f8eb546f6618e7e2f3c867c01e3a6235
eprikhodko <eprikhodkodev@gmail.com>
2022-05-31 10:32:57 +0300

    feat: fetch and render data from API

--- app.vue
+++ app.vue
@@ -1,8 +1,10 @@
-<template>
-  <!-- <div>
-    <NuxtWelcome />
-  </div> -->
+<script setup>
+const { pending, data: currencies } = useLazyFetch(
+  "https://www.binance.com/bapi/asset/v1/public/asset-service/product/currency"
+);
+</script>
 
+<template>
   <div class="px-12 pt-10">
     <div class="max-w-lg mx-auto">
       <header class="flex justify-between">
@@ -25,49 +27,14 @@
 
       <main>
         <div class="grid grid-cols-4 gap-8 mt-8">
-          <div class="bg-background rounded-2xl p-4">
-            <div class="flex items-start">
-              <img src="./assets/img/icons/ETH.svg" alt="eth curreny icon" />
-              <div class="ml-3">
-                <h4 class="font-semibold text-body text-base">Etherium</h4>
-                <h5 class="mt-1 uppercase text-disabled text-sm">Eth</h5>
-              </div>
-            </div>
-            <p class="mt-8 font-semibold text-body text-xl">$0,95</p>
-          </div>
-
-          <div class="bg-background rounded-2xl p-4">
-            <div class="flex items-start">
-              <img src="./assets/img/icons/ETH.svg" alt="eth curreny icon" />
-              <div class="ml-3">
-                <h4 class="font-semibold text-body text-base">Etherium</h4>
-                <h5 class="mt-1 uppercase text-disabled text-sm">Eth</h5>
-              </div>
-            </div>
-            <p class="mt-8 font-semibold text-body text-xl">$0,95</p>
-          </div>
-
-          <div class="bg-background rounded-2xl p-4">
-            <div class="flex items-start">
-              <img src="./assets/img/icons/ETH.svg" alt="eth curreny icon" />
-              <div class="ml-3">
-                <h4 class="font-semibold text-body text-base">Etherium</h4>
-                <h5 class="mt-1 uppercase text-disabled text-sm">Eth</h5>
-              </div>
-            </div>
-            <p class="mt-8 font-semibold text-body text-xl">$0,95</p>
-          </div>
-
-          <div class="bg-background rounded-2xl p-4">
-            <div class="flex items-start">
-              <img src="./assets/img/icons/ETH.svg" alt="eth curreny icon" />
-              <div class="ml-3">
-                <h4 class="font-semibold text-body text-base">Etherium</h4>
-                <h5 class="mt-1 uppercase text-disabled text-sm">Eth</h5>
-              </div>
-            </div>
-            <p class="mt-8 font-semibold text-body text-xl">$0,95</p>
-          </div>
+          <!-- you'll need to handle a loading state -->
+          <div v-if="pending">Loading ...</div>
+          <CurrencyCard
+            v-else
+            v-for="currency in currencies.data"
+            :key="currency.pair"
+            :currency="currency"
+          />
         </div>
       </main>
     </div>
---
+++ components/CurrencyCard.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="bg-background rounded-2xl p-4">
+    <div class="flex items-start">
+      <img src="../assets/img/icons/ETH.svg" alt="eth curreny icon" />
+      <div class="ml-3">
+        <h4 class="font-semibold text-body text-base">
+          {{
+            currency.fullName ? currency.fullName : currency.pair.slice(0, -4)
+          }}
+        </h4>
+        <h5 class="mt-1 uppercase text-disabled text-sm">
+          {{ currency.symbol }}
+        </h5>
+      </div>
+    </div>
+    <p class="mt-8 font-semibold text-body text-xl">{{ currency.rate }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    currency: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+};
+</script>
